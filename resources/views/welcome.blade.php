200882a65c8815c5c5ea86e2aad53bc7a00bf44f
visnicio <vinicius.antonio0712@gmail.com>
2022-11-23 22:47:36 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Vinicius Antonio da Silva | FullStack Developer And Game Designer</title>
+
+        @vite('resources/css/app.css')
+
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Inter:wght@200;400&display=swap');
+            body{
+                font-family: 'Inter', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased bg-gray-900 ">
+        <div class="flex min-h-screen justify-center items-center">
+            <h1 class="text-6xl font-extrabold text-white">HI, I'M VINICIUS ANTONIO</h1>
+        </div>
+    </body>
+</html>
